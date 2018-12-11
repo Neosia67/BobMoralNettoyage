8e6362174eaac30d4f5aabfb6de4800fa057c8d5
Theo Santos <theosnake01@gmail.com>
2018-12-11 22:01:25 +0100

    Models updated to add user informations and created request class

--- website/models.py
+++ website/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 
+# Create your models here.
+
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    mail_addr = models.CharField(max_length=60)
+    phone_number = models.CharField(max_length=20) # Peut-être 12 : +336XXXXXXXX
+    address = models.CharField(max_length=120)
 
-
-# Create your models here.
+class Request(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #TODO Image Field
+    floor = models.PositiveSmallIntegerField(default=0)
+    orientation = (
+    (N, "Nord"),
+    (NNE, "Nord-Nord-Est"),
+    (NE, "Nord-Est"),
+    (ENE, "Est-Nord-Est"),
+    (E, "Est"),
+    (ESE, "Est-Sud-Est"),
+    (SE, "Sud-Est"),
+    (SSE, "Sud-Sud-Est"),
+    (S, "Sud"),
+    (SSO, "Sud-Sud-Ouest"),
+    (SO, "Sud-Ouest"),
+    (OSO, "Ouest-Sud-Ouest"),
+    (O, "Ouest"),
+    (ONO, "Ouest-Nord-Ouest"),
+    (NO, "Nord-Ouest"),
+    (NNO, "Nord-Nord-Ouest")
+    )
+    clean_date = models.DateTimeField("cleaning date")
--- website/urls.py
+++ website/urls.py
@@ -1,31 +1,7 @@
-<<<<<<< ef2a831e1f0af20b03b31c6c0d59acd65000e2eb
-"""nlpf URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from website import views
-
-urlpatterns = [
-    path('', views.home),
-=======
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
->>>>>>> App init
+    path('', views.index, name='index'),
 ]
