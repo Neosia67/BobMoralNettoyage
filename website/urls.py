f4471ba86a72c40d05afa7f53e996dfd26bffbdc
Antoine LEGRAND <legrand.antoine96@gmail.com>
2018-12-12 15:53:39 +0100

    Added all routes

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('myTickets/', views.myTickets, name='my tickets'),
+    path('planning/', views.planning, name='planning'),
+    path('profile/', views.profile, name='profile'),
+    path('signup/', views.signup, name='signup'),
+    path('submittedTickets/', views.submittedTickets, name='submittedTickets'),
+    path('ticket/', views.ticket, name='ticket'),
+    path('ticketForm/', views.ticketForm, name='ticketForm'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html', {})
+
+def myTickets(request):
+	return render(request, 'myTickets.html', {})
+
+def planning(request):
+	return render(request, 'planning.html', {})
+
+def profile(request):
+	return render(request, 'profile.html', {})
+
+def signup(request):
+	return render(request, 'signup.html', {})
+
+def submittedTickets(request):
+	return render(request, 'submittedTickets.html', {})
+
+def ticket(request):
+	return render(request, 'ticket.html', {})
+
+def ticketForm(request):
+	return render(request, 'ticketForm.html', {})
