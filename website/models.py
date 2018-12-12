590c12c772d1a291f40e1307c20074199db5b9bd
Antoine LEGRAND <legrand.antoine96@gmail.com>
2018-12-12 15:53:07 +0100

    Fixed compile issues

--- website/models.py
+++ website/models.py
@@ -3,32 +3,32 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    mail_addr = models.CharField(max_length=60)
-    phone_number = models.CharField(max_length=20) # Peut-être 12 : +336XXXXXXXX
-    address = models.CharField(max_length=120)
+    first_name = models.CharField(max_length=30, default="")
+    last_name = models.CharField(max_length=30, default="")
+    mail_addr = models.CharField(max_length=60, default="")
+    phone_number = models.CharField(max_length=20, default="") # Peut-être 12 : +336XXXXXXXX
+    address = models.CharField(max_length=120, default="") 
 
 class Request(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     #TODO Image Field
     floor = models.PositiveSmallIntegerField(default=0)
     orientation = (
-    (N, "Nord"),
-    (NNE, "Nord-Nord-Est"),
-    (NE, "Nord-Est"),
-    (ENE, "Est-Nord-Est"),
-    (E, "Est"),
-    (ESE, "Est-Sud-Est"),
-    (SE, "Sud-Est"),
-    (SSE, "Sud-Sud-Est"),
-    (S, "Sud"),
-    (SSO, "Sud-Sud-Ouest"),
-    (SO, "Sud-Ouest"),
-    (OSO, "Ouest-Sud-Ouest"),
-    (O, "Ouest"),
-    (ONO, "Ouest-Nord-Ouest"),
-    (NO, "Nord-Ouest"),
-    (NNO, "Nord-Nord-Ouest")
+    ("N", "Nord"),
+    ("NNE", "Nord-Nord-Est"),
+    ("NE", "Nord-Est"),
+    ("ENE", "Est-Nord-Est"),
+    ("E", "Est"),
+    ("SE", "Est-Sud-Est"),
+    ("SE", "Sud-Est"),
+    ("SSE", "Sud-Sud-Est"),
+    ("S", "Sud"),
+    ("SSO", "Sud-Sud-Ouest"),
+    ("SO", "Sud-Ouest"),
+    ("OSO", "Ouest-Sud-Ouest"),
+    ("O", "Ouest"),
+    ("ONO", "Ouest-Nord-Ouest"),
+    ("NO", "Nord-Ouest"),
+    ("NNO", "Nord-Nord-Ouest")
     )
     clean_date = models.DateTimeField("cleaning date")
