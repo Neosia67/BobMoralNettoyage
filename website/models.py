f1c9404779dce1b4225967a4e98dd948cd1bbc3f
I. Marin <issam.marin@gmail.com>
2018-12-16 17:42:39 +0100

    Added building class and improved other classes

--- website/models.py
+++ website/models.py
@@ -1,17 +1,38 @@
+import os
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class User(models.Model):
     first_name = models.CharField(max_length=30, default="")
     last_name = models.CharField(max_length=30, default="")
-    mail_addr = models.CharField(max_length=60, default="")
+    mail_addr = models.EmailField(primary_key=True, max_length=254, default="")
     phone_number = models.CharField(max_length=20, default="") # Peut-être 12 : +336XXXXXXXX
-    address = models.CharField(max_length=120, default="") 
+    address = models.CharField(max_length=120, default="")  # Pourquoi une addresse ?
+
+    @property
+    def get_full_name(self):
+        "Returns the person's full name."
+        return '%s %s' % (self.first_name, self.last_name)
+
+    @property
+    def get_mail_addr(self):
+        "Returns the person's full name."
+        return '%s' % (self.mail_addr)
+
+class Building(models.Model):
+    address = models.CharField(primary_key=True, max_length=254, default="")
+
 
 class Request(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    #TODO Image Field
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    #FIXED Image Field
+    img = models.ImageField(upload_to=settings.MEDIA_ROOT)
     floor = models.PositiveSmallIntegerField(default=0)
     orientation = (
     ("N", "Nord"),
