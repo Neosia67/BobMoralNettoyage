2cbdc5f1a55704704706474404a60a5ba1562a80
Theo Santos <theosnake01@gmail.com>
2018-12-17 21:36:31 +0100

    Pass some arguments to views submittedTickets and ticket

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import User, Building, Request
+
 def home(request):
 	return render(request, 'home.html', {})
 
@@ -19,10 +21,17 @@ def connect(request):
 	return render(request, 'connect.html', {})
 
 def submittedTickets(request):
-	return render(request, 'submittedTickets.html', {})
+	soonest_request_list = Request.objects.order_by('clean_date')[:5]
+	context = {'soonest_request_list': soonest_request_list}
+	return render(request, 'submittedTickets.html', context)
 
-def ticket(request):
-	return render(request, 'ticket.html', {})
+#Ticket detail, raises 404 error if the ticket is not found
+def ticket(request, ticket_id):
+	try:
+		ticket = Request.objects.get(pk=ticket_id)
+	except Request.DoesNotExist:
+		raise Http404("Ticket does not exist")
+	return render(request, 'ticket.html', {'ticket': ticket})
 
 def ticketForm(request):
 	return render(request, 'ticketForm.html', {})
