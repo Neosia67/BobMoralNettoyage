ea5a5f3c2a8e55dcaa8a48a7093908068ecb81f8
Theo Santos <theosnake01@gmail.com>
2018-12-18 18:59:39 +0100

    Better variable name

--- website/views.py
+++ website/views.py
@@ -22,7 +22,7 @@ def connect(request):
 
 def submittedTickets(request):
 	soonest_request_list = Request.objects.order_by('clean_date')[:5]
-	context = {'soonest_request_list': soonest_request_list}
+	context = {'soonest_ticket_list': soonest_ticket_list}
 	return render(request, 'submittedTickets.html', context)
 
 #Ticket detail, raises 404 error if the ticket is not found
@@ -30,5 +30,5 @@ def ticket(request, ticket_id):
 	ticket = get_object_or_404(Ticket, pk=ticket_id)
 	return render(request, 'ticket.html', {'ticket': ticket})
 
-def ticketForm(request):
+def ticketForm(request):	
 	return render(request, 'ticketForm.html', {})
