419dca95ba3c9d7bc342c4d93418b644d50da2c6
I. Marin <issam.marin@gmail.com>
2018-12-18 20:50:55 +0100

    Added sign up page

--- website/urls.py
+++ website/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    url(r'^auth$', views.auth, name='auth'),
+    path('', views.auth, name='auth'),
+    path('home/', views.home, name='home'),
     path('myTickets/', views.myTickets, name='myTickets'),
     path('planning/', views.planning, name='planning'),
     path('profile/', views.profile, name='profile'),
-    path('buildings/', views.buildings, name='buildings'),
-    path('signup/', views.signup, name='signup'),
-    path('connect/', views.connect, name='connect'),
     path('submittedTickets/', views.submittedTickets, name='submittedTickets'),
     path('ticket/<int:ticket_id>/', views.ticket, name='ticket'),
     path('ticketForm/', views.ticketForm, name='ticketForm'),
--- website/views.py
+++ website/views.py
@@ -1,47 +1,54 @@
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Client, Building, Ticket
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+def auth(request):
+	username = request.POST.get('username')
+	password = request.POST.get('password')
+	user = authenticate(request, username=username, password=password)
+	print(username, password, user)
+	if user is not None:
+		login(request, user)
+		return redirect('/home')
+	else:
+		return render(request, 'signIn.html', {})
+
+def signUp(request):
+	return render(request, 'signUp.html', {})
+
+@login_required(login_url='auth')
 def home(request):
 	return render(request, 'home.html', {})
 
+@login_required(login_url='auth')
 def myTickets(request):
 	return render(request, 'myTickets.html', {})
 
+@login_required(login_url='auth')
 def planning(request):
 	return render(request, 'planning.html', {})
 
+@login_required(login_url='auth')
 def profile(request):
 	return render(request, 'profile.html', {})
 
+@login_required(login_url='auth')
 def buildings(request):
 	return render(request, 'buildings.html', {})
 
-def signup(request):
-	return render(request, 'signup.html', {})
-
-def connect(request):
-	email = request.POST.get('email')
-	password = request.POST.get('password')
-	user = authenticate(request, mail_addr=email, password=password)
-
-	if user is not None:
-		login(request, user)
-        # Redirect to a success page.
-		return render(request, 'home.html', {})
-	else:
-        # Return an 'invalid login' error message.
-		return render(request, 'connect.html', {})
-
+@login_required(login_url='auth')
 def submittedTickets(request):
-	soonest_request_list = Request.objects.order_by('clean_date')[:5]
+	soonest_ticket_list = Ticket.objects.order_by('clean_date')[:5]
 	context = {'soonest_ticket_list': soonest_ticket_list}
 	return render(request, 'submittedTickets.html', context)
 
+@login_required(login_url='auth')
 #Ticket detail, raises 404 error if the ticket is not found
 def ticket(request, ticket_id):
 	ticket = get_object_or_404(Ticket, pk=ticket_id)
 	return render(request, 'ticket.html', {'ticket': ticket})
 
+@login_required(login_url='auth')
 def ticketForm(request):
 	return render(request, 'ticketForm.html', {})
