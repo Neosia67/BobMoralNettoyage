e280298ae0096cfb14efdd2a4a3a0df9f759cf6a
I. Marin <issam.marin@gmail.com>
2018-12-19 00:01:42 +0100

    Added Signup page

--- website/urls.py
+++ website/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('myTickets/', views.myTickets, name='myTickets'),
     path('planning/', views.planning, name='planning'),
     path('profile/', views.profile, name='profile'),
+    path('signUp/', views.signUp, name="signUp"),
     path('submittedTickets/', views.submittedTickets, name='submittedTickets'),
     path('ticket/<int:ticket_id>/', views.ticket, name='ticket'),
     path('ticketForm/', views.ticketForm, name='ticketForm'),
