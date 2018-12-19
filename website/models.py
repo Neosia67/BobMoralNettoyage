5d078efe183552edf19a58017646220007969cde
Antoine LEGRAND <legrand.antoine96@gmail.com>
2018-12-19 02:46:45 +0100

    Fixed user calling

--- website/models.py
+++ website/models.py
@@ -23,7 +23,7 @@ class Building(models.Model):
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'client_{0}/{1}'.format(instance.user.email, filename)
+    return 'client_{0}/{1}'.format(instance.user.user.email, filename)
 
 class Ticket(models.Model):
     user = models.ForeignKey(Client, on_delete=models.CASCADE)
